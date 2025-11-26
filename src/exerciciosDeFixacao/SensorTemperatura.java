b5d3f8c6c0d91bbdff28c54d90b25ebc0dd4e5c1
Florindo Albino <florindoalbino252@gmail.com>
2025-11-26 22:54:27 +0100

    Update

--- src/exerciciosDeFixacao/SensorTemperatura.java
+++ src/exerciciosDeFixacao/SensorTemperatura.java
@@ -1,5 +1,74 @@
 package exerciciosDeFixacao;
 
 public class SensorTemperatura {
+    private String identificador;
+    private String local;
+    private Double leituraMinima;
+    private Double leituraMaxima;
+    private Double somaLeituras;
+    private Integer quantidadesLeituras;
+
+    public SensorTemperatura(String identificador, String local) {
+        this.identificador = identificador;
+        this.local = local;
+        this.quantidadesLeituras = 0;
+        this.somaLeituras = 0.0;
+        this.leituraMinima = Double.MIN_VALUE;
+        this.leituraMaxima = Double.MAX_VALUE;
+    }
+
+    public String getIdentificador() {
+        return identificador;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public Double getLeituraMinima() {
+        return leituraMinima;
+    }
+
+    public Double getLeituraMaxima() {
+        return leituraMaxima;
+    }
+
+    public Double getSomaLeituras() {
+        return somaLeituras;
+    }
+
+    public Integer getQuantidadesLeituras() {
+        return quantidadesLeituras;
+    }
+
+    public void registrarLeitura(double valor){
+        if (getQuantidadesLeituras() == 0) {
+            leituraMinima = valor;
+            leituraMaxima = valor;
+        } else {
+            if (valor < leituraMinima) leituraMinima = valor;
+            if (valor > leituraMaxima) leituraMaxima = valor;
+        }
+
+        somaLeituras += valor;
+        quantidadesLeituras++;
+    }   
+    public Double obterMedia(){
+        return getSomaLeituras() / getQuantidadesLeituras();
+    }
+    public String obterIntervalo(){
+        return getLeituraMaxima() + "-" + getLeituraMinima();
+    }
+
+    @Override
+    public String toString(){
+        return "Identificador: " + getIdentificador() + "\n" + 
+                "Local: " + getLocal() + "\n" +
+                "Temperatura minima: " + getLeituraMinima() + "\n" +
+                "Temperatura maxima: " + getLeituraMaxima() + "\n" +
+                "Temperatura media: " + obterMedia() + "\n" + "\n" +
+                "Intervalo de temperatura: " + obterIntervalo();
+
+    }
 
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -4,12 +4,11 @@
  */
 package main;
 
-import exerciciosDeFixacao.Account;
 import exerciciosDeFixacao.ContaTelefonica;
+import exerciciosDeFixacao.SensorTemperatura;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Locale;
 import java.util.Scanner;
 
 /**
@@ -18,38 +17,68 @@ import java.util.Scanner;
  */
 public class Main {
     public static void main(String[] args){  
-        String path = "floras.txt";
+        Scanner sc = new Scanner(System.in);
         
-        String numero = "923456789";
-        String plano = "Pos-pago";
-        
-        int limiteDadoMB = 5000;
-        int dadosConsumidosMB = 0;
-        
-        double valorMensalBase = 10000.0;
-        double valorPorMBExcedente = 5.0;
-        
-        ContaTelefonica conta = new ContaTelefonica(numero,plano,limiteDadoMB,dadosConsumidosMB,valorMensalBase,valorPorMBExcedente);
-        
-        try(BufferedWriter br = new BufferedWriter(new FileWriter(path))){
-            br.write("Exercicio 1 da Mentoria");
-            br.newLine();
-            conta.registrarConsumoDados(2000);
-            conta.registrarConsumoDados(1500);
-            conta.registrarConsumoDados(2500);
-                        
-            br.write(conta.toString());
-            br.newLine();
-            br.write("-----------------------");
-        }catch(IOException e){
-            e.printStackTrace();
-        }
-        
-        
-        
-        
-        
-        
-        
+        int opcao;
+
+        do { 
+            System.out.println("1-Exercicio 1");
+            System.out.println("2-Exercicio 2");
+
+            System.out.println("Digite uma opcao");
+            opcao = sc.nextInt();
+
+            sc.nextLine();
+            switch(opcao){
+                case 1->{
+                    String path = "floras.txt";
+                    
+                    String numero = "923456789";
+                    String plano = "Pos-pago";
+                    
+                    int limiteDadoMB = 5000;
+                    int dadosConsumidosMB = 0;
+                    
+                    double valorMensalBase = 10000.0;
+                    double valorPorMBExcedente = 5.0;
+                    
+                    ContaTelefonica conta = new ContaTelefonica(numero,plano,limiteDadoMB,dadosConsumidosMB,valorMensalBase,valorPorMBExcedente);
+                    
+                    try(BufferedWriter br = new BufferedWriter(new FileWriter(path))){
+                        br.write("Exercicio 1 da Mentoria");
+                        br.newLine();
+                        conta.registrarConsumoDados(2000);
+                        conta.registrarConsumoDados(1500);
+                        conta.registrarConsumoDados(2500);
+                                    
+                        br.write(conta.toString());
+                        br.newLine();
+                        br.write("-----------------------");
+                    }catch(IOException e){
+                        e.printStackTrace();
+                    }
+
+                }
+                case 2->{
+                    System.out.println("Digite o identificador");
+                    String id = sc.nextLine();
+
+                    System.out.println("Digite o local");
+                    String local = sc.nextLine();
+
+                    SensorTemperatura sensor = new SensorTemperatura(id,local);
+                    sensor.registrarLeitura(22.5);
+                    sensor.registrarLeitura(23.0);
+                    sensor.registrarLeitura(24.2);
+                    sensor.registrarLeitura(21.8);
+                    sensor.registrarLeitura(23.5);
+                    
+                    System.out.println(sensor.toString());
+
+                }
+            }
+
+        } while (opcao != 0); 
+        sc.close(); 
     }
 }
